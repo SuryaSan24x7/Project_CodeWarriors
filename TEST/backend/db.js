949cafc6970ef78a8d813de47adc574e66d52562
Suraj Gyaneshwar Waghmare <waghmaresuraj159@gmail.com>
2023-02-22 20:56:01 +0530

    Klean

--- TEST/backend/db.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose")
-
-mongoose.connect("mongodb://127.0.0.1:27017/project")
-    .then(() => {console.log("Database Connected")})
-    .catch((err) => {
-        console.log("Database Connection Failed")
-        console.log(err)
-    })
